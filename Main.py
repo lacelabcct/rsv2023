b4fbcc2266b0bf37f320bc73e09157423b864e2d
Pedro Henrique Trindade <107585625+ttpepeu@users.noreply.github.com>
2023-05-15 15:01:48 -0300

    Add os comentários  e o gráfico

--- Main.py
+++ Main.py
@@ -1,20 +1,27 @@
-from laceRSV23 import dataScraped
+from lacerRSV23 import dataScraped
 import streamlit as st
 import pandas as pd
 from streamlit_chat import message
 from time import *
 from ioBot import io
 from urllib.request import urlopen
+import matplotlib.pyplot as plt
+
+with open('style.css') as s:
+        st.markdown(f'<style>{s.read()}</style>', unsafe_allow_html=True)
 
 st.title('Mackenzie ChatBot')
 
 
 def user():
+        # Transforma as letras em minúscalas e retira o espaço sobrando
         user = st.text_input('Diga um "Oi" para iniciar conversa.').lower().strip()
-        user = user.lower().strip()
         return user
 
+
+
 user_ = user()
+
 if user_  == "":
         pass
 elif user_ in io.bot_answer:
@@ -23,46 +30,56 @@ elif user_ in io.bot_answer:
 elif user_ == 'sim':
         try:
                 select_city = dataScraped.selectCity()
-                message('Selecione as cidades que você deseja analisar:')
+                message('Selecione algumas cidades para análisar:')
                 search_city = st.multiselect(
-                '', select_city)
+                '', select_city) # Lista de multipla escolha que usúario escolher 
+                yearCity = st.selectbox('',('2022','2021','2020','2019'))  # Lista de escolha única
                 frames= []
                 a = [
-                        frames.append(dataScraped.getAPI(
-                        search_city[i], '2022')) for i in range(len(search_city))]
+                        frames.append(
+                        dataScraped.getAPI(
+                        search_city[i], yearCity)) for i in range(len(search_city))]
 
-                result = pd.concat(frames)
+                result = pd.concat(frames) # Juntando os dados selecionados em uma tabela
+                result['Cidade'] = search_city # Adicionando uma coluna das cidades para ser o index
                 st.table(result)
+
+                x = result['Cidade']
+                # Modificando o tipo de valor das colunas
+                result['recursosTransferidosAoGovernoEstadual'] = pd.to_numeric(result['recursosTransferidosAoGovernoEstadual'])
+                result['recursosTransferidosAoMunicipio'] = pd.to_numeric(result['recursosTransferidosAoMunicipio'])
+                result['gastosDiretosGovernoFederalNaLocalidade'] = pd.to_numeric(result['gastosDiretosGovernoFederalNaLocalidade'])
+                result['beneficiosNaLocalidade'] = pd.to_numeric(result['beneficiosNaLocalidade'])
+                
+                x = result['Cidade']
+                y1 = result['recursosTransferidosAoGovernoEstadual']
+                y2 = result['recursosTransferidosAoMunicipio']
+                y3 = result['gastosDiretosGovernoFederalNaLocalidade']
+                y4 = result['beneficiosNaLocalidade']
+                
+                plt.title('recursosTransferidosAoMunicipio')
+                plt.bar(x,y2)
+                plt.show()
+                st.pyplot(plt)
+
         except:
                 pass
         
 else:
+        # Caso o usuário digite algo que não foi programado, exiba esta mensagem
         message('Desculpa, ainda não fui programado pra compreender o que você escreveu')
 
+# Função agrega o dado inserido para uma planilha
+def add_sheets(data):
+        response = urlopen(
+                f'https://docs.google.com/forms/d/e/1FAIpQLScRqU6yRw2Ykln-42Dj2_NfORcwbsUc_pfadE7p3KPMZtY5AQ/formResponse?&submit=Submit?usp=pp_url&entry.2034641448=x&entry.2146359889={data}')
+        html = response.read()
+        return html
 try:
-        if user_ == "":
+        # Adicionando as palavras que o usuário digitou no banco de dados
+        if user_ == "": 
                 pass
         else:
-                response = urlopen(
-                f'https://docs.google.com/forms/d/e/1FAIpQLScRqU6yRw2Ykln-42Dj2_NfORcwbsUc_pfadE7p3KPMZtY5AQ/formResponse?&submit=Submit?usp=pp_url&entry.2034641448=x&entry.2146359889={user_}'
-                        )
-                html = response.read()
-
-        if search_city[0] == '':
-                pass
-        else:
-                response = urlopen(
-                f'https://docs.google.com/forms/d/e/1FAIpQLScRqU6yRw2Ykln-42Dj2_NfORcwbsUc_pfadE7p3KPMZtY5AQ/formResponse?&submit=Submit?usp=pp_url&entry.2034641448=x&entry.2146359889={search_city[0]}'
-                        )
-                html = response.read()
-
-        if search_city[1] == '':
-                pass
-        else:
-                response = urlopen(
-                f'https://docs.google.com/forms/d/e/1FAIpQLScRqU6yRw2Ykln-42Dj2_NfORcwbsUc_pfadE7p3KPMZtY5AQ/formResponse?&submit=Submit?usp=pp_url&entry.2034641448=x&entry.2146359889={search_city[1]}'
-                        )
-                html = response.read()
-
+              add_sheets(user_)
 except:
         pass
