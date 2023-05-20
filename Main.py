aa7354d0053b3d36c44feed0f70d4d44b95ec2e7
lacelabcct <128186141+lacelabcct@users.noreply.github.com>
2023-05-20 03:01:31 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -95,10 +95,10 @@ elif user_ == 'sim':
                 plt.show()
                 st.pyplot(plt)                
                 
-                url= 'https://docs.google.com/forms/d/e/1FAIpQLSe3k4qby8XCLb4ABrZ972PW_VK4PS3aJo_qCEX-nfDsYiaMeg/formResponse?&submit=Submit?usp=pp_url&entry.705323696=' + str(y2)
+                url= 'https://docs.google.com/forms/d/e/1FAIpQLSe3k4qby8XCLb4ABrZ972PW_VK4PS3aJo_qCEX-nfDsYiaMeg/formResponse?&submit=Submit?usp=pp_url&entry.705323696=' + str(search_city)
 
                 if st.button('Confirmar 👇'):
-                    SendMAIL(y2)
+                    SendMAIL(str(search_city))
                     response = urlopen(f'{url}')
                     html = response.read()               
 
