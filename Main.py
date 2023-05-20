66bde0b6b21b9f3d92ed9528ba20fe41a62a1e60
lacelabcct <128186141+lacelabcct@users.noreply.github.com>
2023-05-20 02:56:39 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -97,11 +97,11 @@ elif user_ == 'sim':
                 
                 txtNome = st.text_input("Digite seu nome completo 👇")
                 if txtNome:
-                    url= 'https://docs.google.com/forms/d/e/1FAIpQLSe3k4qby8XCLb4ABrZ972PW_VK4PS3aJo_qCEX-nfDsYiaMeg/formResponse?&submit=Submit?usp=pp_url&entry.705323696=' + str(txtNome)
-                if st.button('SALVAR 👇'): 
-                    SendMAIL(y2)
+                url= 'https://docs.google.com/forms/d/e/1FAIpQLSe3k4qby8XCLb4ABrZ972PW_VK4PS3aJo_qCEX-nfDsYiaMeg/formResponse?&submit=Submit?usp=pp_url&entry.705323696=' + str(txtNome)
+
+                if st.button('Confirmar 👇'):
                     response = urlopen(f'{url}')
-                    html = response.read()                    
+                    html = response.read()               
 
         except:
                 pass
