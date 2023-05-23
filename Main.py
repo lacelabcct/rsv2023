90b9c3595345a481a2fb20457ec73c6a0d54f4ee
Pedro Henrique Trindade <107585625+ttpepeu@users.noreply.github.com>
2023-05-23 10:41:12 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -23,7 +23,7 @@ def SendMAIL(conteudo):
     password = "efoqfcwiorncteas"
     msg['From'] = "prof.massaki@gmail.com" 
     email = st.text_input('Digite seu E-mail:')
-    msg['To'] = email
+    msg['To'] = str(email)
     msg['Subject'] = "Assunto"
     #file = "Python.pdf"
     # attach image to message body 
